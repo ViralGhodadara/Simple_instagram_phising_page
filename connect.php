0646836bbe8bd0fdf54f5448eaddb9e701047a16
Viral Ghodadara <59829404+ViralGhodadara@users.noreply.github.com>
2020-10-02 12:46:25 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,17 @@
 <?php
 
-	$servername = "HOST NAME";
-	$password = "PASSWORD";
-	$username = "USERNAME";
-	$databased = "DATABASE";
+	$servername = "localhost";
+	$password = "";
+	$username = "dbusername";
+	$databased = "dbname";
 
 	$connection = mysqli_connect($servername, $username, $password, $databased);
+
+// 	if($connection) {
+// 		echo "connection can successfully.....";
+// 	} else {
+// 		echo "connection cannot successfully please try again";
+// 	}
 
 	
 ?>
